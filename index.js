184b66ad4dd9a60624e479363fb0bf79e1ad6bf2
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-07-24 17:52:00 +0600

    Getting single college data done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qx5eerd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,6 +30,15 @@ async function run() {
       const result = await collegeCollection.find().toArray();
       res.send(result);
     });
+
+     // Getting single college data
+     app.get("/college/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await collegeCollection.findOne(query);
+      res.send(result);
+    });
+
 
     // Getting reviews data
     app.get("/reviews", async (req, res) => {
